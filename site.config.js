eb7f52fd9c198303a3d02e7632a8d45e71d9cf12
saelogy <saenna01@gmail.com>
2024-02-21 14:14:56 +1100

    avatar

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "sae",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "hobbyist everything",
     bio: "here to play around with things",
     email: "",
